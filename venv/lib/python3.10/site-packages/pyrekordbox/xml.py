605d46d0e9df9bf969eace7aeda2a22894a79cf7
tseitz <tdseitz10@gmail.com>
2024-10-18 09:55:36 -0500

    Remove files that should be ignored

--- venv/lib/python3.10/site-packages/pyrekordbox/xml.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Author: Dylan Jones
-# Date:   2022-04-10
-
-from .utils import warn_deprecated as _warn_deprecated
-from .rbxml import *  # noqa: F401, F403
-
-_warn_deprecated("pyrekordbox.xml", "pyrekordbox.rbxml", remove_in="0.4.0")
